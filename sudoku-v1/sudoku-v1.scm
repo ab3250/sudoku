84afdb00948701aa3e6286bacec6a0f337c7db8d
ab3250 <ab3250@live.com>
2020-11-13 22:48:53 -0500

    version 1.3

--- sudoku-v1/sudoku-v1.scm
+++ sudoku-v1/sudoku-v1.scm
@@ -4,26 +4,18 @@
 
 (define (solve)
   (let/ec return
-    (let row-loop ((row 0))
-      (if (not (eqv? 9 row))
-          (begin	  
-            (let col-loop ((col 0))
-              (if (not (eqv? 9 col))
-                  (begin
-                    (if (eqv?(array-ref grid row col) 0)
-                        (let num-loop ((num 1))
-                          (if (not (eqv? 10 num))
-                              (begin
-                                (if (possible? row col num)
-                                    (begin
-                                      (array-set! grid num row col)
-                                      (solve)
-                                      (unless(position-9x9 0 (array->list (array-contents grid)))(print-grid))
-                                      (array-set! grid 0 row col)))
-                                (num-loop (+ 1 num)))
-                              (return)))#f)
-                    (col-loop (+ col 1)))#f))
-            (row-loop (+ row 1)))#f))))
+    (for 0 8 (lambda(row)
+	       (for 0 8 (lambda(col)
+			  (if (eqv?(array-ref grid row col) 0)
+                              (for 1 9 (lambda(num)
+					 (if (possible? row col num)
+					     (begin
+					       (array-set! grid num row col)
+					       (solve)
+					       (unless(position-9x9 0 (array->list (array-contents grid)))(print-grid))
+					       (array-set! grid 0 row col)))
+					 (when (eqv? num 9)
+					   (return)))))))))))
 (print-grid)
 (solve)
 (newline)
--- sudoku-v2/sudoku-v2.scm
+++ sudoku-v2/sudoku-v2.scm
@@ -3,15 +3,16 @@
 (include "../library/sudoku-library.scm")
 
 (define (solve)
-  (define zero (position-9x9 0 (array->list (array-contents grid))))
-  (if (not(not zero))
-      (let ((row (car zero))(col (cdr zero)))
-        (for 1 9 (lambda(num)                            
-                   (if (possible? row col num)
-                       (begin
-                         (array-set! grid num row col)                       
-                         (if (not(solve)) (begin (print-grid)(exit)))
-                         (array-set! grid 0 row col))))))#f))
+  (let ((next-zero (position-9x9 0 (array->list (array-contents grid)))))
+    (if (not(eqv? next-zero #f))
+	(let ((row (car next-zero))(col (cdr next-zero)))
+          (for 1 9 (lambda(num)
+                     (if (possible? row col num)
+			 (begin
+                           (array-set! grid num row col)
+                           (if (not(solve)) (print-grid))
+                           (array-set! grid 0 row col))))))
+	#f)))
 (print-grid)
 (solve)
 
